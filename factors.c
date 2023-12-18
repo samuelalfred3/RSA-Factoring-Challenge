85c3168c5cb8516749cd5c01d3617a9b6d7b1a22
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2023-12-18 22:40:05 +0300

    Added some files

---
+++ factors.c
@@ -0,0 +1,35 @@
+#include "factor.h"
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fptr;
+	size_t count;
+	ssize_t line;
+	char *buffer = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: factor <filename>\n");
+		exit(EXIT_FAILURE);
+	}
+	fptr = fopen(argv[1], "r");
+	if (fptr == NULL)
+	{
+		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while((line = getline(&buffer, &count, fptr)) != -1)
+	{
+		long num = atol(buffer);
+		factorize(num);
+	}
+	free(buffer);
+	fclose(fptr);
+	return (0);
+}
---
+++ factors.h
@@ -0,0 +1,12 @@
+#ifndef _FACTOR_H_
+#define _FACTOR_H_
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int factorize(char *buffer);
+
+#endif /* _FACTOR_H_ */
